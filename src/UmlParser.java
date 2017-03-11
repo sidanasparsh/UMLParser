66c335f2e7fc434c64fcddb98db36d7c35ee03d7
Sparsh <sidanasparsh@gmail.com>
2017-03-10 19:44:48 -0800

    Error handeleing for input arguments

--- src/UmlParser.java
+++ src/UmlParser.java
@@ -2,6 +2,9 @@ public class UmlParser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		if(args.length!=2){
+			System.out.println("Error: Please enter input file and output file paths!");
+			System.exit(0);
+		}
 	}
 }
