d55be3f83e541866f19a5f7379ab42ab8a7c93be
Sparsh <sidanasparsh@gmail.com>
2017-04-01 17:39:06 -0700

    Added functions for generating sequence diagram

--- src/SequenceDiagramGenerator.java
+++ src/SequenceDiagramGenerator.java
@@ -1,4 +1,11 @@
 public class SequenceDiagramGenerator {
 	private static String sourceFile;
 	private static String destinationPath;
+  SequenceDiagramGenerator(String sourceFile, String destinationPath){
+    this.sourceFile=sourceFile;
+    this.destinationPath=destinationPath;
+  }
+  public static void generateDiagramImage(String outputFilePath){
+
+  }
 }
