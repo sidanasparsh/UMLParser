44a6417a3b9238d0b0801729553b0441c11a938f
Sparsh <sidanasparsh@gmail.com>
2017-04-08 20:15:14 -0700

    added parsing methods

--- src/DiagramGenerator.java
+++ src/DiagramGenerator.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import japa.parser.ast.CompilationUnit;
+
 public class DiagramGenerator {
 	private static String sourceFile;
 	private static String destinationPath;
+	ArrayList<CompilationUnit> compilationArray;
+
 	DiagramGenerator(String sourceFile, String destinationPath){
 		this.sourceFile=sourceFile;
 		this.destinationPath=destinationPath;
 	}
-	public static void generateDiagramImage(String outputFilePath){
+	   public void parse() {
+	        compilationArray = getCompilationList(sourceFile);
+	    }
+	public static ArrayList<CompilationUnit> getCompilationList(String str){
+        ArrayList<CompilationUnit> compilationArray = new ArrayList<CompilationUnit>();
 
+		return compilationArray;
 	}
 }
