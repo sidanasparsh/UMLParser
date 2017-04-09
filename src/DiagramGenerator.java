d2fea3322b708ffda50b2c054b3ec5f91ba00f7f
Sparsh <sidanasparsh@gmail.com>
2017-04-08 20:40:05 -0700

    Added correct access modifires

--- src/DiagramGenerator.java
+++ src/DiagramGenerator.java
@@ -7,9 +7,9 @@ import japa.parser.ast.CompilationUnit;
 public class DiagramGenerator {
 	private static String sourceFile;
 	private static String destinationPath;
-	ArrayList<CompilationUnit> compilationList;
+	private static ArrayList<CompilationUnit> compilationList;
 	private static String stringyUML;
-	HashMap<String, Boolean> map;
+	private static HashMap<String, Boolean> map;
 
 	DiagramGenerator(String sourceFile, String destinationPath){
 		this.sourceFile=sourceFile;
@@ -27,11 +27,10 @@ public class DiagramGenerator {
 		map=new HashMap<String, Boolean>();
 	}
 	private String getParsedString(CompilationUnit compilationUnit) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	public static ArrayList<CompilationUnit> getCompilationList(String str){
-        ArrayList<CompilationUnit> compilationList = new ArrayList<CompilationUnit>();
+		compilationList = new ArrayList<CompilationUnit>();
 
 		return compilationList;
 	}
