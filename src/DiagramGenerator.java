3d1795d6f8a8123e7a5599f172aa17fe6ac548df
Sparsh <sidanasparsh@gmail.com>
2017-04-08 20:41:57 -0700

    Fiexd indentation errors

--- src/DiagramGenerator.java
+++ src/DiagramGenerator.java
@@ -15,14 +15,14 @@ public class DiagramGenerator {
 		this.sourceFile=sourceFile;
 		this.destinationPath=destinationPath;
 	}
-	  public void parse() {
-		  compilationList = getCompilationList(sourceFile);
-	        for (CompilationUnit compilationUnit : compilationList){
-						if(getParsedString(compilationUnit)!=null)
-	            stringyUML += getParsedString(compilationUnit);}
-	        makeCompilationMap(compilationList);
-	        System.out.println("YUML String: " + stringyUML);
-	    }
+	public void parse() {
+	  compilationList = getCompilationList(sourceFile);
+        for (CompilationUnit compilationUnit : compilationList){
+					if(getParsedString(compilationUnit)!=null)
+            stringyUML += getParsedString(compilationUnit);}
+        makeCompilationMap(compilationList);
+        System.out.println("YUML String: " + stringyUML);
+    }
 	private void makeCompilationMap(ArrayList<CompilationUnit> compilationList) {
 		map=new HashMap<String, Boolean>();
 	}
@@ -31,7 +31,6 @@ public class DiagramGenerator {
 	}
 	public static ArrayList<CompilationUnit> getCompilationList(String str){
 		compilationList = new ArrayList<CompilationUnit>();
-
 		return compilationList;
 	}
 }
