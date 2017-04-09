30e2b69f37c02d394fd87a471d87934c42f4ad22
Sparsh <sidanasparsh@gmail.com>
2017-04-08 20:18:33 -0700

    Added required parsing methods

--- src/DiagramGenerator.java
+++ src/DiagramGenerator.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,14 +8,23 @@ public class DiagramGenerator {
 	private static String sourceFile;
 	private static String destinationPath;
 	ArrayList<CompilationUnit> compilationArray;
+	private static String stringyUML;
 
 	DiagramGenerator(String sourceFile, String destinationPath){
 		this.sourceFile=sourceFile;
 		this.destinationPath=destinationPath;
 	}
-	   public void parse() {
+	  public void parse() {
 	        compilationArray = getCompilationList(sourceFile);
+	        for (CompilationUnit cu : compilationArray)
+	            stringyUML += getParsedString(cu);
+
 	    }
+	private String getParsedString(CompilationUnit compilationUnit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public static ArrayList<CompilationUnit> getCompilationList(String str){
         ArrayList<CompilationUnit> compilationArray = new ArrayList<CompilationUnit>();
 
